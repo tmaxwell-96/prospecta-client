94ba9b3caaf5abcca5c27ac3fe98f3a78caf1a00
Taylor Maxwell <taylor.w.maxwell@gmail.com>
2023-11-27 10:46:37 -0800

    Remove warnings for deployment

--- src/components/DeleteCompany/DeleteCompany.jsx
+++ src/components/DeleteCompany/DeleteCompany.jsx
@@ -1,14 +1,11 @@
 import React, { useState } from "react";
 import Modal from "react-modal";
 import "./DeleteCompany.scss";
-import axios from "axios";
 
 Modal.setAppElement("#root");
 
 const DeleteCompany = ({ company, deleteCompany }) => {
-  const baseURL = process.env.REACT_APP_BASE_URL;
   const [modalIsOpen, setIsOpen] = useState(false);
-  const token = sessionStorage.getItem("JWTtoken");
 
   function openModal() {
     setIsOpen(true);
